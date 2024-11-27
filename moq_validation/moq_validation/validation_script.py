c9c925babc0d772cefbb91487d56aa2da915fc0a
The Voice <57120898+momardia54@users.noreply.github.com>
2024-11-27 05:20:20 -0500

    Update validation_script.py
    Take into consideration the case where doc have no attribute item to ensure this script does not cause error for doctypes that are not concerned.

--- moq_validation/moq_validation/validation_script.py
+++ moq_validation/moq_validation/validation_script.py
@@ -5,6 +5,10 @@ def validate_moq(doc, method):
     """
     Validate if all items in the Quotation have a quantity >= MOQ.
     """
+    # Check if there are items in the document
+    if not doc.items:
+        return  # Do nothing if there are no items
+
     for item in doc.items:
         # Fetch the item's custom MOQ value
         custom_moq = frappe.db.get_value("Item", item.item_code, "custom_moq")
@@ -16,4 +20,3 @@ def validate_moq(doc, method):
                     item.item_code, item.qty, custom_moq
                 )
             )
-
